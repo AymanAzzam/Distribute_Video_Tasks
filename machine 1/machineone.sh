22d672f07b2a5783dcd25f55d50d6fc3eeb16177
Ayman Azzam <aymanazzam63@gmail.com>
2020-02-23 23:30:36 +0200

    Update scripts sockets

--- machine 1/machineone.sh
+++ machine 1/machineone.sh
@@ -21,7 +21,7 @@ for (( i=0; i<$n; i++))
 do
 	echo "in the foor loop number " $i
 	python stage1.py $port_in $port_out	&
-	if [ $(($i % 2))  -eq 0  -a $(($i -ne 0))]
+	if [ $(($i % 2))  -eq 1 ]
 	then
 		port_out=$(($port_out + 1))
 	fi
--- machine 2/machinetwo.sh
+++ machine 2/machinetwo.sh
@@ -7,6 +7,7 @@ echo "socket 1 is initially $socket1_collect"
 socket2_collect=$(((($socket1_collect)) + (($novertwo))))
 echo "socket 2 is initially $socket2_collect"
 
+socket2_collect_temp=$socket2_collect
 
 for (( i=0; i<$novertwo; i++))
 do
@@ -15,8 +16,8 @@ do
 	socket2_collect=$(($socket2_collect + 1))
 done
 
-socket1_stage2=$socket2_collect
-socket2_stage2=$(((($socket2_collect)) + (($novertwo))))
+socket1_stage2=$socket2_collect_temp
+socket2_stage2=$(((($socket1_stage2)) + (($novertwo))))
 echo "socket stage is initially $socket2_stage2"
 
 for (( i=1; i<=n; i++))
@@ -27,3 +28,5 @@ do
 		socket1_stage2=$(($socket1_stage2 + 1))
 	fi
 done
+
+python collector2.py $socket2_stage2
